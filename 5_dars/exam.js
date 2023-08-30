ba8942014016a982ec4ae45dcae693f2d1606c7d
raximjon2010 <rahimjonxujaqulov9@gmail.com>
2023-08-30 15:28:25 +0500

    For Loop | Basic

---
+++ 5_dars/exam.js
@@ -0,0 +1,127 @@
+
+// Bu yerdan qaysini tekshirmoqchi bolsangiz command ochib tekshirishingiz mumkun Bu faqat kodlar 
+// aralashib ketmasligi uchun
+
+// ------------------------------1 - exam--------------------------------------------------------------------
+// Juft va toqga ajratish
+// for(i = 1 ; i < 10 ; i++){
+//     if (i % 2 === 0) {
+//         console.log("Juft son |" + i);
+//     }
+//     else{
+//         console.log("Toq son |" + i);
+//     }
+// }
+// ------------------------------2 - exam--------------------------------------------------------------------
+// let text = "Webbrain Academya"
+// let countCapitalA = 0;
+// let countSmallA = 0;
+// for (i = 0 ; text.length; i++){
+//     if(text[i] === "A"){
+//         // countCapitalA++
+//     }
+//     else if(text[i] === 'a'){
+//         // countSmallA++
+//     }
+//     console.log('Katta "A" harfi soni:', countCapitalA);
+//     console.log('Kichik "a" harfi soni:', countSmallA);
+// }
+// ------------------------------3 - exam--------------------------------------------------------------------
+// So'zni teskarisni ogrib berish
+// let Text = "Rahimjon Xo'jaqulov"
+// for (i = "Webbrain Academy".length - 1 ; i >= 0 ; i--){
+//     console.log(Text[i]);
+// }
+// ------------------------------4 - exam--------------------------------------------------------------------
+// k va n butun sonlari berilgan (n > 0) k sonini n marta chiquvchi programma tuzilsin
+// let k = 10;
+// let n = 5;
+// function chiqarish(k,n) {
+//     for (let i = 0 ; i < n ; i++) {
+//         console.log(k);
+//     }
+// }
+// chiqarish(k,n)
+// ------------------------------ 5 - exam --------------------------------------------------------------------
+//  har bir konfetni narxi berilgan haqiqiy sonda 1,2,3,4,5,6,7,8,9,10,-
+//  konfetni narxini chiqaruvchi dastur tuzulsin
+// for(i = 1 ; i < 10 ; i++){
+    // console.log("Konfetni", i + " kgmi Narxi:" + i + "0 000 so'm" );
+// }
+// ------------------------------ 6 - exam --------------------------------------------------------------------
+//  Bir kilogramm konfetni narxi berilgan haqiqiy son 0.1,0.2,0.3,0.4,0.5,0.6,0.7, 0.8,0.9,
+//  konfetni narxini chiqaruvchi programma tuzulsin
+//  for (i = 1 ; i < 2; i++){
+//     let price = 0.1
+//     switch(price){
+//         case 0.1 : console.log("0.1 gr konfetni narxi 1000 so'm");
+//         case 0.1 : console.log("0.2 gr konfetni narxi 2000 so'm");
+//         case 0.1 : console.log("0.3 gr konfetni narxi 3000 so'm");
+//         case 0.1 : console.log("0.4 gr konfetni narxi 4000 so'm");
+//         case 0.1 : console.log("0.5 gr konfetni narxi 5000 so'm");
+//         case 0.1 : console.log("0.6 gr konfetni narxi 6000 so'm");
+//         case 0.1 : console.log("0.7 gr konfetni narxi 7000 so'm");
+//         case 0.1 : console.log("0.8 gr konfetni narxi 8000 so'm");
+//         case 0.1 : console.log("0.9 gr konfetni narxi 9000 so'm");
+//         case 0.1 : console.log("0.1 gr konfetni narxi 10000 so'm");
+
+//     }
+//  }
+// ------------------------------ 7 - exam --------------------------------------------------------------------
+//  Bir kilogramm konfetni narxi berilgan haqiqiy son 1.2,1.3,1.4,1.5,1.6,1.7,1.8, 2.9,
+//  konfetni narxini chiqaruvchi programma tuzulsin
+
+// let konfetNarxlar = [1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 2.9];
+// for (let i = 0; i < konfetNarxlar.length; i++) {
+//   console.log(`Konfetning ${i+1} kilogrammi narxi: ${konfetNarxlar[i]} so'm`);
+// }
+
+// ------------------------------ 8 - exam --------------------------------------------------------------------
+console.log("=========2 Karra=========");
+
+for(i = 1 ; i <= 10; i++){
+    let karra2 = 2;
+    console.log(`${karra2} * ${i} = ` , karra2 * i );
+}
+console.log("==========3 Karra=========");
+for (i = 1 ; i <= 10 ; i++){
+    let karra3 = 3;
+    console.log(`${karra3} * ${i} = ` , karra3 * i);
+}
+
+console.log("==========4 Karra==========");
+
+for (i = 1 ; i <= 10 ; i++){    
+    let karra4 = 4;
+    console.log(`${karra4} * ${i} = ` , karra4 * i);
+}
+console.log("==========5 Karra==========");
+
+for (i = 1 ; i <= 10 ; i++){
+    let karra5 = 5;
+    console.log(`${karra5} * ${i} = `, karra5 * i);
+}
+
+console.log("==========6 Karra==========")
+for(i = 1; i <= 10; i++){
+    let karra6 = 6;
+    console.log(`${karra6} * ${i} = `, karra6 * i );
+}
+console.log("==========7 Karra==========")
+for(i = 1 ; i <= 10 ; i++ ){
+    let karra7 = 7;
+    console.log(`${karra7} * ${i} = `, karra7 * i);
+}
+console.log("==========8 Karra==========")
+for ( i = 1 ; i <= 10; i++){
+    let karra8 = 8;
+    console.log(`${karra8} * ${i} = ` , karra8 * i);
+}
+console.log("==========9 Karra==========")
+for(i = 1 ; i <= 10 ; i++){
+    let karra9 = 9;
+    console.log(`${karra9} * ${i} = ` , karra9 * i);
+} 
+
+// Bu yerdan qaysini tekshirmoqchi bolsangiz command ochib tekshirishingiz mumkun Bu faqat kodlar 
+// aralashib ketmasligi uchun 
---
+++ 5_dars/script.js
@@ -0,0 +1,33 @@
+
+
+
+
+// let i = 1; - Bu initial Expiression - Bu for loopga boshlangich qiymat biriktirish 
+// i <= 10; - Bu condition bu qachon true bolsa keyin block ichidagi kod ishlaydi Agar true bolmasa ishlamaydi.
+// i++ - Uptade Expiression Bu mana shu condition true bolsa keyin uning ichidagi kod ishlaydi keyin i++ qiymatni birga oshiradi.
+// for(let i = 10; i >= 1; i--){
+//     console.log(i);
+// } // bu raqamlarni teskarisini chiqarish uchun ishlatiladi. 
+// let sum = "Webbrain Academy    "
+// for(i = sum.length -1; i >= 0; i--){
+//     console.log(sum[i]);
+// } Bu stringni teskarisini chiqarish uchun ishlatiladi length -1 bu length 1 dan boshlangani uchun shuni 0dan qilib quyibdi
+
+// String =>
+// index  doim 0dan boshlanadi
+// length doim 1 dan boshlanadi
+// console.log(str[i]); bu har bitta harfni chiqaradi
+// console.log(str); Shart boyicha stringni toliq chiqaradi
+// console.log(i); Bu raqamlarni chiqaradi.
+
+// Var bilan letni farqi var bu global let bilan const block scoped 
+// varni  biz for loppni ichida yozdikmi demak uni tashqarisida yozsak bu hammasini qiymatini bitta qilib chiqarib beradi
+// yani 1 dan 10 gacha raqam yozilgan bolsa varni for loop blocidan tashqarida yozsak 11 chiqaradi
+// Bu umuman kerak bolmagan qiymat bu ishlatilmaydi desak ham boladi
+// let esa block scoped for loopni ichidagi scopedan tashqaraida umuman ishlataolmaymiz is not defined beradi
+// const bu esa ozgartirib bolmaydi i++ qiymatni birga ochiradi shuning uchun const ishlamaydi
+
+
+
+
+
