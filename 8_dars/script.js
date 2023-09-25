bc0d2f86e1795cb58dc87c4d4db16edc0c9ca10d
raximjon2010 <rahimjonxujaqulov9@gmail.com>
2023-09-25 14:49:39 +0300

    Number

---
+++ 8_dars/script.js
@@ -0,0 +1,54 @@
+// Number 
+// Math built in methods
+// console.log(Math.pow(3, 2)); //=> sonni darajaga kotarish, 2ta qiymat oladi, 1-son, 2-nechchi darajaga kotarish
+// console.log(Math.sqrt(25));// => sonni ildiz ostida chiqarish
+// console.log(Math.cbrt(64)); //=> sonni uchinchi darajali ildiz ostidan chiqaradi
+// console.log(Math.min(20, 45, 2,3,4,5,1,4,)); //=> minimum sonni oladi
+// console.log(Math.max(20, 45, 2)); => maximum sonni oladi
+// console.log(Math.abs(-100));// => faqat musbat son qaytardi
+// console.log(parseInt(Math.random() * 100)); //=> tasodifit son qaytaradi
+// console.log(Math.round(10.5)); //=> sonni yaxlitlaydi, qoldiq qism 5 va undan yuqori bulsa, 1 qushib beradi
+// console.log(Math.floor(5.7)); //=> faqat butun qismni qaytaradi
+// console.log(Math.ceil(10.1)); //=> qoldiq qism bulsa, songa bir qushadi
+
+// number methods
+// let son = 12.12;
+// console.log(Number.isInteger(son)); //=> son butun yoki butun emasligini tekshiradi, boolean qiymat qaytaradi
+// console.log(typeof Number.parseInt(son)); //=> sonni faqat butun qismini qaytaradi va typeof ni xar doim numberga ugiradi
+// console.log(parseInt(son)); => sonni faqat butun qismini qaytaradi va typeof ni xar doim numberga ugiradi
+// console.log( +son); //=> faqatgina typeof ni number ga ugiradi
+// console.log( +son.toFixed(1)); //=> sonni yaxlitlaydi, va xar doim typeof string qaytaradi, qoldiq qismdan xoxlagan qismni ajratib olishimiz mumkin
+// console.log((son.toFixed()));
+
+// console.log(typeof 12345);
+// console.log(10 + 10);
+// console.log(10 - 5);
+// console.log(10 / 5);
+// console.log(10 * 5);
+// console.log(11 % 5);
+// console.log(2 4);
+// console.log(64 (1/2));
+// console.log(64 ** 0.5);
+// console.log(son++);
+// console.log(++son);
+
+
+
+// pow 
+// sqr
+// cbrt
+//min
+// max
+// abs
+// random
+// round
+// floor
+// ceil
+// IsInteger
+// parseInt()
+// toFixed
+
+// Match 
+// let str = "We33443543233435432341231b3fg56vcbt7"
+// let str2 = str.match(/[0-9]/g)
+// console.log(str2);
---
+++ 8_dars/task.js
@@ -0,0 +1,156 @@
+// -----------------------------------------------1task---------------------------------------------------
+// Random programmasi
+// let random1 = "";
+// for (let i = 1; i <= 1; i++) {
+//     let sum = random1
+//     sum = Math.floor(Math.random() * 1000000000)
+//     if (sum === sum) {
+//         // console.log(sum.padStart(9 , "+998");
+//     }
+
+//     console.log(sum);
+// }
+// -----------------------------------------------2task---------------------------------------------------
+// Functionda raqamlarni qo'shish
+// function qoshish(num1, num2) {
+//     return num1 + num2
+// }
+// console.log(qoshish(5,3));
+
+// let x = function qoshish(num1, num2) {
+//     return num1 + num2
+// }
+// console.log(x(2,3));\
+
+// let myFunction = (num1 , num2 ) => {
+    // return num1 + num2
+// }
+// console.log(myFunction(2,5));
+// -----------------------------------------------3task---------------------------------------------------
+// Functionda raqamlarni ayirish
+// function ayirish(num1, num2) {
+//     return num1 - num2
+// }
+// console.log(ayirish(10,7));
+
+// let x = function ayirish(num, num2){
+//     return num - num2
+// }
+// console.log(x(10,2));
+
+// let ayirish = (num1 , num2) => {
+//     return num1 - num2
+// }
+// console.log(ayirish(12,6));
+// -----------------------------------------------4task---------------------------------------------------
+// functionda raqamlarni bolish
+// function bolish(num1 ,num2) {
+// if (num2 !== 0) {
+//     return num1 / num2
+// }
+// else{
+//     console.log("0 ga bolish mukun emas");
+// }
+// }
+// console.log(bolish(15 , 0));
+// let x = function bulish(num1,num2){
+//     if (num2 !== 0) {
+//         num1 / num2
+//     }
+//     else{
+//         console.log("0 ni bolib bolmaydi");
+//     }
+// }
+// console.log(x(10,5));
+// let bolish2 =  (num1 , num2) => {
+//     if (num2 !== 0) {
+//         num1 / num2
+//     }
+//     else{
+//         console.log("0 ni bolib bolmaydi");
+//     }
+// }
+// console.log(bolish2(25,5));
+// -----------------------------------------------5task---------------------------------------------------
+// functionda raqamlarni kopaytirish
+// function kopaytirish(num1, num2) {
+//     return num1 * num2
+// }
+// console.log(kopaytirish(2,3));
+// let x = function kopaytirish (num1 , num2){
+//     return num1 * num2
+// }
+// console.log(x(10,4));
+// let kopaytirish = (num1 , num2) => {
+//     return num1 * num2
+// }
+// console.log(kopaytirish(12,556));
+// -----------------------------------------------6task---------------------------------------------------
+// Functionda raqamlarni kvadrati chiqarish
+// function kvadrati(num) {
+//     return Math.pow(num,2)
+// }
+// console.log(kvadrati(10,));
+// -----------------------------------------------7task---------------------------------------------------
+// Sonni kubini xisoblash
+// function kubi(kub) {
+//     return Math.pow(kub, 3)
+// }
+// console.log(kubi(3));
+// -----------------------------------------------8task---------------------------------------------------
+// sonni ildiz ostidan chiqarish
+// function ildiz(num1,num2){
+//     return Math.sqrt(25,5)
+// }
+// console.log(ildiz());
+// -----------------------------------------------9task---------------------------------------------------
+// Min ni topish
+// let x = function(num1 , num2){
+//     return Math.min(12,23,34,5,56,7,3,75,8)
+// }
+// console.log(x());
+// -----------------------------------------------10task---------------------------------------------------
+// Maxni topish
+// let max = () =>{
+//     return Math.max(1,21,3,4,53,56,24,13)
+// }
+// console.log(max());
+// -----------------------------------------------11task---------------------------------------------------
+// Musbat qilib chiqarish
+// function abs() {
+//     return Math.abs(-100)
+// }
+// console.log(abs());
+// -----------------------------------------------12task---------------------------------------------------
+// random 
+// let random = function(){
+//     return parseInt(Math.random() * 100)
+// }
+// console.log(random());
+// -----------------------------------------------13task---------------------------------------------------
+// round 
+// let round = () =>{
+//     return Math.round(5.6)
+// }
+// console.log(round());
+// -----------------------------------------------14task---------------------------------------------------
+// floor
+// function floor(){
+// return Math.floor(5.10)
+// }
+// console.log(floor());
+// -----------------------------------------------15task---------------------------------------------------
+// ceil 
+// let ceil = () =>{
+//     return Math.ceil(4.001)
+// }
+// console.log(ceil());
+
+
+
+
+
+
+
+
+
